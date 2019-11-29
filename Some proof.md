ed56c89cdc45369cf150c0c135a281c7e790d13b
Rejur <wwwfeng321@gmail.com>
2019-11-30 00:48:27 +0800

    complete SLR_1_Pi_given_x

---
+++ Some proof.md
@@ -0,0 +1,20 @@
+# SLR_Homework
+
+##### Exercise 1.2:
+
+Because A and x are known, so problem is $\mathop{argmin}_{\Pi\in P}||y-\Pi c||_2$, It is mean I want $||y - c_\Pi||_2^2$'s answer is minimum. $||y-c_\Pi||_2^2 = \Sigma_1^m(y_i-c_{\Pi i})^2=\Sigma_1^m(y_i^2+c_{\Pi i}^2) -2\Sigma_1^m y_ic_{\Pi i}$. By sequence inequality, we know when When order is order， $\Sigma_1^my_ic_{\Pi i}$ is maximum，so the answer is minimum. 
+
+(Unique) Because out of order is smaller than bigger, and In Probability $c_{\Pi i}==c_{\Pi j}$ is zero probability event,  In computer, I think it is a impossible event by use matlab try lots of times, so the solution is unique.
+
+```
+Algorithm 1
+Input y, A, x
+Output Pi
+Start:
+	sort(y) and record their origin location s1
+	sort(c) and record their origin location s2
+	Generate Pi from s1 and s2
+End
+```
+
+so the complexity is $O(mlogm)$.
--- code/SLR_1_Pi_given_x.m
+++ code/SLR_1_Pi_given_x.m
@@ -1,6 +1,28 @@
 function [Pi_idx] = SLR_1_Pi_given_x(A, y, x)
 % Pi_idx is a m-dimensional vector such that Pi*A = A(Pi_idx, :)
-
-Pi_idx = zeros(m, 1);
+C = A * x;
+m = size(A, 1);
+% disp(m);
+yAddPos = [transpose(y);(1:m)];
+yAddPos = transpose(yAddPos);
+cAddPos = [transpose(C);(1:m)];
+cAddPos = transpose(cAddPos);
+yAddPos = sortrows(yAddPos);
+cAddPos = sortrows(cAddPos); % 排序后的第二列，作为 PI * A = I，代表它们原来的位置， 通过第二列进行置换可以得到正序数组。
+% tmp1_idx = zeros(m, 1);
+tmp2_idx = zeros(m, 1);
+tmp3_idx = zeros(m, 1);
+% disp(yAddPos);
+for index = 1:m
+    % disp(cAddPos(index, 2))
+    % tmp1_idx(cAddPos(index , 2) , 1) = index;
+    tmp2_idx(yAddPos(index , 2) , 1) = index; % 通过根据位置反向填写index，是告诉I这个值去哪才可以得到A即PI^-1。
+    tmp3_idx(index, 1) = cAddPos(index, 2);
+end
+% disp(tmp1_idx);
+% disp(tmp2_idx);
+% disp(tmp3_idx);
+Pi_idx = tmp3_idx(tmp2_idx,:);
+% disp(C(Pi_idx,:));
 end
 
