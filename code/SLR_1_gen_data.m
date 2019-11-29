f723db52c5e577e5d04fb591968ad1ade9ee8be1
Rejur <wwwfeng321@gmail.com>
2019-11-29 18:01:53 +0800

    Complete SLR_1_gen_data

--- code/SLR_1_gen_data.m
+++ code/SLR_1_gen_data.m
@@ -1,7 +1,23 @@
 function [A, y, x] = SLR_1_gen_data(m, n, sigma, shuffled_ratio)
 % y = Pi*A*x + sigma*noise
-A = 0;
-y = 0;
-x = 0;
+shuffledNum = round(shuffled_ratio * m / 100);
+shuffledNum = min(m, max(2, shuffledNum));
+% disp(shuffledNum)
+A = rand([m n]);
+x = rand([n 1]);
+Ax = A * x;
+disp(A);
+disp(Ax);
+% disp((1 : shuffledNum))
+while true
+    rndPerm = randperm(shuffledNum);
+    if sum(rndPerm == (1 : shuffledNum)) == 0
+        break;
+    end
+end
+% disp(rndPerm)
+Ax(1:shuffledNum) = Ax(rndPerm);
+disp(Ax);
+y = Ax + normrnd(0, sigma^2, [m, 1]);
 end
 
